2fca6644a1b75cf0ca99945c1c94f34282065131
Gordianus <poszukiwacz.gordianus@gmail.com>
2024-09-15 23:52:50 +0200

    Build the home page. Add background image with fill, placeholder='blur', className='object-cover ovject-top'

--- app/layout.js
+++ app/layout.js
@@ -24,12 +24,12 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <body
-        className={`${josefinFont.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased`}
+        className={`${josefinFont.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased relative`}
       >
         <Header />
 
         <div className="flex-1 px-8 py-12">
-          <main className=" max-w-7xl bg-red-500 mx-auto ">{children}</main>
+          <main className=" max-w-7xl mx-auto ">{children}</main>
         </div>
       </body>
     </html>
