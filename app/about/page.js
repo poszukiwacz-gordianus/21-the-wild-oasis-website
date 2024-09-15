03b8487836feaeda60764028aab2018fb506f107
Gordianus <poszukiwacz.gordianus@gmail.com>
2024-09-15 15:57:43 +0200

    Start adding navigation between pages

--- app/about/page.js
+++ app/about/page.js
@@ -1,3 +1,10 @@
+import Navigation from "../components/Navigation";
+
 export default function Page() {
-  return <h1>About The Wild Oasis</h1>;
+  return (
+    <div>
+      <Navigation />
+      <h1>About The Wild Oasis</h1>
+    </div>
+  );
 }
