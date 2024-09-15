8ecda2935f33d37230d274b4ac4658e7202b93e1
Gordianus <poszukiwacz.gordianus@gmail.com>
2024-09-15 16:24:51 +0200

    Create layout, add logo and navigation, specify page title using metadata

--- app/about/page.js
+++ app/about/page.js
@@ -1,9 +1,6 @@
-import Navigation from "../components/Navigation";
-
 export default function Page() {
   return (
     <div>
-      <Navigation />
       <h1>About The Wild Oasis</h1>
     </div>
   );
--- app/page.js
+++ app/page.js
@@ -1,10 +1,6 @@
-import Link from "next/link";
-import Navigation from "./components/Navigation";
-
 export default function Page() {
   return (
     <div>
-      <Navigation />
       <h1>The wild oasis. Welcome to paradise.</h1>
     </div>
   );
