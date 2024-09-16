1e59d5fdde534a996340e718453d4de2402d28a0
Gordianus <poszukiwacz.gordianus@gmail.com>
2024-09-16 23:53:41 +0200

    Add highlighting to side navigation link

--- app/_components/SideNavigation.js
+++ app/_components/SideNavigation.js
@@ -1,4 +1,7 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import {
   CalendarDaysIcon,
@@ -27,13 +30,17 @@ const navLinks = [
 ];
 
 function SideNavigation() {
+  const pathname = usePathname();
+
   return (
     <nav className="border-r border-primary-900">
       <ul className="flex flex-col gap-2 h-full text-lg">
         {navLinks.map((link) => (
           <li key={link.name}>
             <Link
-              className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200`}
+              className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 ${
+                pathname === link.href ? " bg-primary-900" : ""
+              }`}
               href={link.href}
             >
               {link.icon}
