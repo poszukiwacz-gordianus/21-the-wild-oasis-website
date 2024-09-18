4fb256a4458ac3317c614de39bc4337c882236da
Gordianus <poszukiwacz.gordianus@gmail.com>
2024-09-18 23:50:46 +0200

    Removed “no flag” placeholder for users without a flag.

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -32,11 +32,13 @@ function UpdateProfileForm({ guest, children }) {
       <div className="space-y-2">
         <div className="flex items-center justify-between">
           <label htmlFor="nationality">Where are you from?</label>
-          <img
-            src={countryFlag}
-            alt="Country flag"
-            className="h-5 rounded-sm"
-          />
+          {countryFlag && (
+            <img
+              src={countryFlag}
+              alt="Country flag"
+              className="h-5 rounded-sm"
+            />
+          )}
         </div>
 
         {children}
